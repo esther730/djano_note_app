c440ecdf015ed9476af05112eb6df113a250bdb6
Esther Su <esther730@berkeley.edu>
2021-02-03 14:12:35 -0800

    init from udemy

---
+++ NotelyProject/urls.py
@@ -0,0 +1,20 @@
+"""
+Definition of urls for NotelyProject.
+"""
+
+from datetime import datetime
+from django.urls import path
+from django.contrib import admin
+from django.contrib.auth.views import LoginView, LogoutView
+from app import forms, views
+
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('new-note/', views.create_note, name='create_note'),
+    path('note/<int:pk>/detail/', views.note_detail, name='note_detail'),
+    path('note/<int:pk>/edit/', views.edit_note, name='edit_note'),
+    path('note/<int:pk>/delete/', views.delete_note, name='delete_note'),
+]
+
+
---
+++ env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!F:\Udemy-Courses\FullStack Django and Python MasterClass\CodeBase\Notely\NotelyProject\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
